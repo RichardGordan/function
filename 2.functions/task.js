86707cb38991e1d6e50d50d4fa883c6d7d991eee
Pavel Shetkov <fertym99@gmail.com>
2021-09-27 19:19:01 +0300

    function first

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,20 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > i){
+      max = arr[i];
+    }else if(arr[i] < i){
+      min = arr[i];
+    };
+    sum =+ arr[i];
+    avg = sum / arr.length
+  }
 
-  // Ваш код
+ 
 
   return { min:min, max:max, avg:avg };
 }
