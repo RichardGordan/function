56d5cb40b42072a97f9865243ef7024ef0afa1c7
Pavel Shetkov <fertym99@gmail.com>
2021-09-28 16:54:06 +0300

    e

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,48 @@
+'use strict'
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  if (a < 0) {
+    return false;
+  }
+  let x1;
+  let x2;
+  let discriminant = (b ** 2 - 4 * a * c);
+
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+     arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+     
+     
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a)); 
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+    
+
+  }
+
   return arr; // array
 }
+	
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let S = amount - contribution; 
+  let P = percent / 100 / 12; 
+  let date1 = new Date();
+  let n = (date.getFullYear()*12 + date.getMonth()) - (date1.getFullYear()*12 + date1.getMonth());
+  
+  if ((percent < 0 ||  contribution < 0 || amount < 0 || n < 0 ) || 
+  (isNaN(percent) === true|| isNaN(contribution) === true || isNaN(amount) === true || isNaN(date) === true)) {
+      return "Ошибка данные введены неверно";
+  }
 
-  // код для задачи №2 писать здесь
+  let monthAmount = S * ( P + P / (((1 + P) ** n) - 1));
+   totalAmount = monthAmount * n;
 
-  return totalAmount;
+  return Number(totalAmount.toFixed(2));
+
+
 }
+
+
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -5,31 +5,40 @@ function getArrayParams(arr) {
   let sum = 0;
   let avg = 0;
   for(let i = 0; i < arr.length; i++){
-    if(arr[i] > i){
+    if(arr[i] > max){
       max = arr[i];
-    }else if(arr[i] < i){
+    }else if(arr[i] < min){
       min = arr[i];
-    };
+    }
     sum =+ arr[i];
-    avg = `${(sum / arr.length).to fixed(2)}`
   }
-
- 
+  
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  let sum = 0;
+for(let i = 0; i < arr.length; i++){
+  sum += arr[i];
+}
+  
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = arrOfArr[0]];
+  worker(arr, (arrOfArr[i] => 
+    for(let i = 0; i < arrOfArr.length; i++){
+      if(arrOfArr[i] > max ){
+        max = arrOfArr[i];
+      }
+    }
+  
+
 
   // Ваш кода
   // for ...
