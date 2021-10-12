35bb2c00c77b124a4a6a91f08e63e2936a555143
Pavel Shetkov <fertym99@gmail.com>
2021-10-12 17:06:24 +0300

    Obj  and classes

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,12 +1,14 @@
+
 function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
     this.age = age;
+    
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  return subjectName
+   this.subject = subjectName;
 }
 Student.prototype.addMark = function (mark) {
   if(this.marks === undefined){ 
@@ -18,7 +20,12 @@ Student.prototype.addMark = function (mark) {
 
 }
 Student.prototype.addMarks = function(...marks){
-  this.marks = [...rest];
+  if(this.marks === undefined){
+    this.marks = [...marks];
+}else{
+  this.marks.push(...marks);
+}
+  
 
 }
 
@@ -32,8 +39,7 @@ Student.prototype.getAverage = function(){
 Student.prototype.exclude = function(reason){
   delete this.setSubject;
   delete this.marks;
-  return this.reason
+   this.exluded = reason;
 }
 let student1 = new Student;
 let studen2 = new Student;
-// ваш код для остальных методов
--- 5.classes/task.js
+++ 5.classes/task.js
@@ -7,20 +7,21 @@ class PrintEditionItem {
         this.type = null;
     };
     fix (){
-        return this.state * 1.5
+         this.state * 1.5
     }
 set state(number){
     if(number === 0){
-        this.state = 0;
+        this._state = 0;
     }else if(number > 100){
-        this.state = 100;
+        this._state = 100;
     }else{
-        this.state = number;
-    }
-    get state(){
-        return this.state;
+        this._state = number;
     }
 }
+    get state(){
+        return this._state;
+    }
+
     }
 class Magazine extends PrintEditionItem {
     constructor(){
@@ -76,3 +77,39 @@ class DetectiveBook extends Book {
 
     }
 };
+
+class Library{
+    constructor(name,books){
+        this.name = name;
+        this.books = [];
+    }
+    addBook(book){
+        if(book.state >= 30){
+            return this.books.push(book)
+        }else{
+            return false
+        }
+    }
+    findBookBy(type, value){
+        let resultSerach = this.books.find(book => book.type === value );
+        return (typeof resultSerach === 'object') ? resultSerach : null
+    }
+    giveBookByName(bookName){
+        let searchBook = this.books.find(book => book.name === bookName);
+        if(typeof searchBook === 'object'){
+            this.books.splice(this.books.indexOf(searchBook),1)
+            return searchBook
+      }else{
+          return null
+      }
+    }
+}
+
+class Student{
+    #storage;
+    constructor(name){
+        this.name = name;
+        this.#storage = {};
+    }
+    }
+    
