809294fce417fd2abca0b12efe9271bbd4b7f93d
Pavel Shetkov <fertym99@gmail.com>
2021-10-16 15:16:33 +0300

    async

---
+++ 8.async/task.js
@@ -0,0 +1,78 @@
+class AlarmClock{
+    constructor(){
+        this.alarmCollection = [];
+        this.timerId = null;
+    }
+    addClock(time,callback,id){
+        if(typeof id === 'undefined'){
+            throw new Error('error text');
+        }else if(this.alarmCollection.find(bell => bell.id === id) !== 'undefiend'){
+            return console.error('Alarm s already standing');
+        }
+        return this.alarmCollection.push(time,callback,id);
+
+    }
+    removeClock(id){
+        this.alarmCollection.filter(function(){
+            let repeatedID = this.id;
+            alarmCollection.forEach(bell, i => {
+                if(bell.id === repeatedID){
+                    alarmCollection.splice(i,1);
+                    return true
+                }else{
+                    return false
+                }
+                
+            });
+        })
+
+        
+    }
+    getCurrentFormattedTime(){
+        const time = new Date;
+        const HH = time.getHours;
+        const MM = time.getMinutes;
+        return `${HH} : ${MM};`
+        
+
+    }
+    start(){
+        let checkClock = (clock) => {
+            let alarm = this.getCurrentFormattedTime();
+            if(clock.time === alarm){
+                return clock.callback
+            }
+        }
+        if(this.timerId == null){
+            setInterval(() => {
+                this.alarmCollection.forEach(clock =>  checkClock(clock));
+
+            },1000)
+            
+        }
+        return 
+
+    }
+    stop(){
+        if(this.timerId !== null){
+            clearInterval(this.timerId);
+            return this.timerId = null
+        }
+    }
+    printAlarms(){
+        return this.alarmCollection.forEach(time, id => console.log(`${this.time} + ${this.id}`) );
+    }
+
+clearAlarms(){
+    this.stop() 
+    return this.alarmCollection = [];
+}
+}
+let AlertClock = new AlarmClock ;
+AlertClock.addClock('11:25', () => console.log ('New week!'), 1);
+AlertClock.addClock('15:30', () => console.log ('New week!!!'), 2);
+AlertClock.removeClock(2);
+AlertClock.addClock('00:50', () => console.log ('Go sleep'), 3);
+AlertClock.start();
+AlertClock.stop();
+AlertClock.printAlarms()
